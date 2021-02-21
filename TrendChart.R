d4773b5ae4d10073773b4baf5f7fe6dfa13ba276
tommyli123@gmail.com <tommyli123@gmail.com>
2021-02-21 15:11:57 -0800

    init

--- TrendChart.R
+++ TrendChart.R
@@ -5,8 +5,34 @@ library(maps)
 
 incarceration_trends <- read.csv("c:\\Users\\tommy\\project\\incarceration-trends\\incarceration_trends.csv")
 
+# Step 1
+white_and_poc_all_years <- incarceration_trends %>%
+  group_by(year) %>%
+  summarize(total_white_jail_pop = sum(white_jail_pop, na.rm=TRUE),
+            total_aapi_jail_pop = sum(aapi_jail_pop, na.rm=TRUE),
+            total_black_jail_pop = sum(black_jail_pop, na.rm=TRUE),
+            total_latinx_jail_pop = sum(latinx_jail_pop, na.rm=TRUE),
+            total_native_jail_pop = sum(native_jail_pop, na.rm=TRUE),
+            total_other_race_jail_pop = sum(other_race_jail_pop, na.rm=TRUE)
+  ) %>%
+  select(year, white=total_white_jail_pop, aapi=total_aapi_jail_pop, black=total_black_jail_pop, 
+       latinx=total_latinx_jail_pop, native=total_native_jail_pop, 
+       other_race=total_other_race_jail_pop)  
+
+white_and_poc_all_years_summary <- white_and_poc_all_years %>%
+  gather(key=race, value=jail_pop, white, black, latinx, aapi, native, other_race) %>%
+  select(year, race, jail_pop)
+
+ggplot(data = white_and_poc_all_years_summary, aes(x=year, y=jail_pop, group=race)) +
+  geom_line(aes(linetype="solid", color=race, size=1)) +
+  geom_point() +
+  guides(color=guide_legend(override.aes = list(size=3))) +
+  theme(legend.position = "right")
+
+# Step 2 
+
 white_and_poc <- incarceration_trends %>%
-  filter(year >= 2000) %>%
+  filter(year >= 1985) %>%
   group_by(year) %>%
   summarize(total_white_jail_pop = sum(white_jail_pop, na.rm=TRUE),
             total_aapi_jail_pop = sum(aapi_jail_pop, na.rm=TRUE),
@@ -24,6 +50,7 @@ white_and_poc <- incarceration_trends %>%
 #  select(year, race, jail_pop)
 View(white_and_poc)
 
+# Step 3 
 white_vs_non_white_summary <- white_and_poc %>%
   gather(key=race, value=jail_pop, white, non_white) %>%
   select(year, race, jail_pop)
@@ -34,6 +61,7 @@ ggplot(data = white_vs_non_white_summary, aes(x=year, y=jail_pop, group=race)) +
   guides(color=guide_legend(override.aes = list(size=3))) +
   theme(legend.position = "right")
 
+# Step 4
 white_vs_each_race_summary <- white_and_poc %>%
   gather(key=race, value=jail_pop, white, black, latinx, aapi, native, other_race) %>%
   select(year, race, jail_pop)
@@ -43,12 +71,12 @@ ggplot(data = white_vs_each_race_summary, aes(x=year, y=jail_pop, group=race)) +
   guides(color=guide_legend(override.aes = list(size=3))) +
   theme(legend.position = "right")
 
+# Step 5
 
-
-# This shows the top 5 states that has the highest latinx incarceration rate since 2000, 
+# This shows the top 5 states that has the highest latinx incarceration rate since 1985, 
 # it consistently shows FL, NY, AZ, NJ, MA
 state_latinx <- incarceration_trends %>%
-  filter(year >= 2000) %>%
+  filter(year >= 1985) %>%
   group_by(year, state) %>%
   summarize(total_latinx_jail_pop = sum(latinx_jail_pop, na.rm=TRUE)) %>%
   select(year, state, total_latinx_jail_pop) %>%
@@ -60,9 +88,10 @@ ggplot(data = state_latinx, aes(x=year, y=total_latinx_jail_pop, group=state)) +
   guides(color=guide_legend(override.aes = list(size=3))) +
   theme(legend.position = "right")
 
-# This shows the top 5 states that has the highest black incarceration rate since 2000, 
+# Step 6
+# This shows the top 5 states that has the highest black incarceration rate since 1985, 
 state_black <- incarceration_trends %>%
-  filter(year >= 2000) %>%
+  filter(year >= 1985) %>%
   group_by(year, state) %>%
   summarize(total_black_jail_pop = sum(black_jail_pop, na.rm=TRUE)) %>%
   select(year, state, total_black_jail_pop) %>%
@@ -76,7 +105,7 @@ ggplot(data = state_black, aes(x=year, y=total_black_jail_pop, group=state)) +
   theme(legend.position = "right")
 
 
-
+# Step 7
 
 # full join to find common top 5 states and which are not
 state_latinx_black <- full_join(state_latinx, state_black, by=c("year", "state")) %>%
